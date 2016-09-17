45c2efde33dd73dcbf71dad3e48e355a37425819
unknown <kabeerdarocker@gmail.com>
2016-09-17 12:59:28 +0545

    test

---
+++ tests/features/EventsTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class EventsTest extends MasterTestCase
+{
+    /** @test */
+    public function it_should_be_able_to_create_future_events()
+    {
+
+    }
+}
