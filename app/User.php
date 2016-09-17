a73480605da56ca4a77547467ad507cf9ab59819
unknown <kabeerdarocker@gmail.com>
2016-09-17 12:53:19 +0545

    user model

--- app/User.php
+++ app/User.php
@@ -2,12 +2,14 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
+use Hootlex\Friendships\Traits\Friendable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens,Friendable, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'employee_id','mobile','name','designation','branch','bio','email','password','remember_token'
+        'employee_id','mobile','name','designation','branch','bio','email','password','remember_token','created_at','updated_at'
         ];
 
     /**
@@ -26,4 +28,37 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public static function findByEmployeeId($empId)
+    {
+        return self::employeeId($empId)->first();
+    }
+
+    public static function findOrFailByEmployeeId($employeeID)
+    {
+        return self::employeeId($employeeID)->firstOrFail();
+    }
+
+    public function scopeEmployeeId($query, $employeeID)
+    {
+        return $query->where('employee_id',$employeeID);
+    }
+
+    /**
+       * Get all of the user's likes.
+       */
+    public function likes()
+    {
+      return $this->hasMany(Like::class);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'employee_id';
+    }
 }
