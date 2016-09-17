87d2217ed1ca279126b2c3cee7640af4710fc061
unknown <kabeerdarocker@gmail.com>
2016-09-17 12:56:05 +0545

    api routes

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,33 @@
 <?php
 
+use App\Post;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+Route::post('posts/{post}/comment', 'API\CommentsController@create')->name('leave.comments');
+
+Route::get('posts/{post}/comments', function(Post $post, Request $request) {
+    $time = Carbon::createFromTimestamp( $request->get('timestamp', Carbon::now()->timestamp) );
+    $comments = $post->comments()
+                    ->with('user')
+                    ->where('created_at','<', $time)
+                    ->take(5)
+                    ->get();
+    if( ! $comments->count() )
+    {
+        return response()->json(['code' => '77', 'message' => 'No Record found.']);
+    }
+    $result = $comments->map(function($comment) use($time){
+         return [
+                'profile_picture'  =>  '/images/32.png',
+                'user_name' => $comment->user->name,
+                'timestamp' => $time,
+                'message' => $comment->message
+            ];
+    });
+
+    return response()->json(['last_timestamp' => $result->first()['timestamp'], 'users' => $result]);
+});
