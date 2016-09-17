d429a1476b377b12cf678ac5da1a3cc1322da236
unknown <kabeerdarocker@gmail.com>
2016-09-17 12:52:43 +0545

    web routes

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,30 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+    // DB::connection()->enableQueryLog();
+    // $user = App\User::first();
+    // $results = $user->posts()->with(['comments' => function($query){ $query->latest()->take(2); }])->paginate();
+    // $queries = DB::getQueryLog();
+    // dd($queries, $results);
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::get('/', 'HomeController@index');
+Route::get('create/oauth', function ()
+{
+    return view('welcome');
+});
+Route::get('home', 'HomeController@index');
+Route::get('@{employee}/posts/{post}', 'PostsController@show')->name('posts.show');
+Route::post('@{employee}/posts', 'PostsController@create')->name('posts.create');
+Route::get('@{employee}', 'FriendsController@show')->name('friends.show');
+Route::get('friends/requests', 'FriendsController@index')->name('friends.index');
+Route::post('friends/requests', 'FriendsController@store')->name('friends.requests');
+Route::post('friends/accept', 'FriendsController@update')->name('friends.accept');
+Route::post('friends/decline', 'FriendsController@delete')->name('friends.decline');
+Route::get('oauth/client/create', function() {
+    return view('oauth.create-client');
+});
